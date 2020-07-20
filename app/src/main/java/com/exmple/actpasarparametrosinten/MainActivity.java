8d38e175e2416219bf5058f25fc66d006650d1ee
Angel Albinagorta Vargas <43548855+albinagorta@users.noreply.github.com>
2020-07-20 10:59:00 -0500

    Initial commit

---
+++ app/src/main/java/com/exmple/actpasarparametrosinten/MainActivity.java
@@ -0,0 +1,13 @@
+package com.exmple.actpasarparametrosinten;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
